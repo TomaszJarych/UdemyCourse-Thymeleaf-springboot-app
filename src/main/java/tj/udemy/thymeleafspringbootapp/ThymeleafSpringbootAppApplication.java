791d2afb199e20969655083544f737dd8a0be861
Tomasz Jarych <tjarych@gmail.com>
2019-01-22 18:48:00 +0100

    Init commit

---
+++ src/main/java/tj/udemy/thymeleafspringbootapp/ThymeleafSpringbootAppApplication.java
@@ -0,0 +1,14 @@
+package tj.udemy.thymeleafspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafSpringbootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafSpringbootAppApplication.class, args);
+    }
+
+}
+
